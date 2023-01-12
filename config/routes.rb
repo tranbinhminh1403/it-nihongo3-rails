76a9acec4171fcf48ea628acc54ac827c29032e3
tranbinhminh1403 <diobrando1403@gmail.com>
2023-01-12 16:21:07 +0700

    add_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'static_pages/home'
+  root 'static_pages#home'
+  # get '/users/sign_out'
+  devise_for :users 
+    devise_scope :user do  
+      get '/users/sign_out' => 'devise/sessions#destroy'     
+   end
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
